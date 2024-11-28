0979f63b7bc5421748db7dc7a4d47f685936d8f8
ice <xykxyk2008@163.com>
2024-11-28 23:25:11 +0800

    init

--- server/internal/model/config.go
+++ server/internal/model/config.go
@@ -8,10 +8,10 @@ import (
 type Config struct {
 	ggorm.Model `gorm:"embedded"` // embed id and time
 
-	Name        string `gorm:"column:name;type:varchar(32);NOT NULL" json:"name"`                // 配置名称
-	Description string `gorm:"column:description;type:varchar(255);NOT NULL" json:"description"` // 描述
-	Key         string `gorm:"column:key;type:varchar(64);NOT NULL" json:"key"`                  // 配置键
-	Value       string `gorm:"column:value;type:text;NOT NULL" json:"value"`                     // 配置值
+	Name        string `gorm:"column:name;type:text;NOT NULL" json:"name"`               // 配置名称
+	Description string `gorm:"column:description;type:text;NOT NULL" json:"description"` // 描述
+	Key         string `gorm:"column:key;type:text;NOT NULL" json:"key"`                 // 配置键
+	Value       string `gorm:"column:value;type:text;NOT NULL" json:"value"`             // 配置值
 }
 
 // TableName table name
